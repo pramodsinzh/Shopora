e7319800444ac30c9e82bafdfa2dce365c8ff841
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-13 09:27:29 +0545

    feat: add new UI components including Carousel, Checkbox, Collapsible, Command, Dialog, Input Group, Input, Label, Popover, Radio Group, Scroll Area, Separator, Table, Textarea, Tooltip, and utility functions

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-10 bg-red-300'>
+    <div className='p-10 bg-shop_light_pink'>
       <h2 className='text-xl font-semibold'>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ut necessitatibus non minus possimus! Aspernatur fuga expedita ipsam delectus ut.</p>
     </div>
