a55579cce35a4dab60d88550d8cab7d8d342a71b
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-13 08:51:22 +0545

    Add global styles and update Home component layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='p-10 bg-red-300'>
+      <h2 className='text-xl font-semibold'>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ut necessitatibus non minus possimus! Aspernatur fuga expedita ipsam delectus ut.</p>
     </div>
   )
 }
