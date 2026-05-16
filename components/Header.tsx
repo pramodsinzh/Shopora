b787128f49d3cee41bc987457399ad4183db5a9e
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-16 20:08:31 +0545

    feat: add SearchBar, CartIcon, FavoriteButton, and SignIn components to enhance header functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import Container from './Container'
 import Logo from './Logo'
 import HeaderMenu from './HeaderMenu'
+import SearchBar from './SearchBar'
+import CartIcon from './CartIcon'
+import FavoriteButton from './FavoriteButton'
+import SignIn from './SignIn'
 
 const Header = () => {
   return (
     <header className='bg-white py-5'>
-        <Container className='flex items-center justify-between'>
-            <Logo />
-            <HeaderMenu />
-            <div className="">Others</div> 
-        </Container>
+      <Container className='flex items-center justify-between'>
+        <Logo />
+        <HeaderMenu />
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteButton />
+          <SignIn />
+        </div>
+      </Container>
     </header>
   )
 }
