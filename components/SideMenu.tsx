37015f7a0e170d13de65669ed68dd156be6dc1aa
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-19 09:12:16 +0545

    feat: integrate useOutsideClick hook to enhance SideMenu functionality

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,6 +5,7 @@ import { headerData } from '@/constants/data';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import SocialMedia from './SocialMedia';
+import { useOutsideClick } from '@/hook';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -13,9 +14,10 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
     const pathname = usePathname()
+    const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
     return (
         <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full text-white/80 bg-black/50 shadow-xl ${isOpen ? "translate-x-0" : "-translate-x-full"} hoverEffect`}>
-            <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
+            <div ref={sidebarRef} className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
                 <div className="flex items-center justify-between gap5 ">
                     <Logo className='text-white' spanDesign='group-hover:text-white' />
                     <button onClick={onClose} className='hover:text-shop_light_green hoverEffect'><X /></button>
---
+++ components/SocialMedia.tsx
@@ -0,0 +1,58 @@
+import React from 'react' 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { BaggageClaimIcon } from 'lucide-react';
+
+interface Props {
+    className?: string;
+    iconClassName?: string;
+    tooltipClassName?: string;
+}
+
+const socialLink = [
+    {
+        title: "YouTube",
+        href: "#",
+        icon: <BaggageClaimIcon className='w-5 h-5' />
+    },
+    {
+        title: "LinkedIn",
+        href: "#",
+        icon: <BaggageClaimIcon className='w-5 h-5' />
+    },
+    {
+        title: "Facebook",
+        href: "#",
+        icon: <BaggageClaimIcon className='w-5 h-5' />
+    },
+]
+
+const SocialMedia = ({ className, iconClassName, tooltipClassName }: Props) => {
+    return <TooltipProvider>
+        <div className={cn("flex items-center gap-3.5", className)}>
+            {socialLink.map((item) => (
+                <Tooltip key={item?.title}>
+                    <TooltipTrigger
+                        render={
+                            <Link
+                                href={item.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={cn(
+                                    "p-2 border rounded-full hover:text-white hover:border-shop_light_green hoverEffect",
+                                    iconClassName
+                                )}
+                            />
+                        }
+                    >
+                        {item.icon}
+                    </TooltipTrigger>
+                    <TooltipContent className={cn("bg-white text-darkColor font-semibold", tooltipClassName)}>{item?.title}</TooltipContent>
+                </Tooltip>
+            ))}
+        </div>
+    </TooltipProvider>
+}
+
+export default SocialMedia
