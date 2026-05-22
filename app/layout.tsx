f83371e95c0238480a12e8b58452df9138a8cfb3
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-22 20:11:45 +0545

    feat: add HomeBanner component and update Home page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={cn("font-sans", geist.variable)} >
         <body className="font-poppins antialiased">
-          <Header />
-          {children}
-          <Footer />
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </body>
       </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Container from '@/components/Container'
+import Container from '@/components/Container'  
+import HomeBanner from '@/components/HomeBanner'
 import React from 'react'
 
 const Home = () => {
   return (
     <Container className='bg-shop_light_pink'>
-      <h2 className='text-xl font-semibold'>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ut necessitatibus non minus possimus! Aspernatur fuga expedita ipsam delectus ut.</p>
+      <HomeBanner />
     </Container>
   )
 }
