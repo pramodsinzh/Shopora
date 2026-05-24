f50a1f2430a8bed7295528b900eb46e1da811d44
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-24 07:52:21 +0545

    feat: integrate Sanity CMS and set up initial project structure

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+
+export const metadata: Metadata = {
+  title: "Shopora",
+  description: "Shopora online store is a one-stop destination for all your shopping needs. We offer a wide range of products, from fashion and electronics to home goods and more. With our user-friendly interface and secure payment options, shopping with us is easy and convenient. Explore our collection today and find the perfect items to suit your style and needs.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,14 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/lib/utils";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils"
+import { Geist } from "next/font/google"
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
-export const metadata: Metadata = {
-  title: "Shopora",
-  description: "Shopora online store is a one-stop destination for all your shopping needs. We offer a wide range of products, from fashion and electronics to home goods and more. With our user-friendly interface and secure payment options, shopping with us is easy and convenient. Explore our collection today and find the perfect items to suit your style and needs.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className={cn("font-sans", geist.variable)} >
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return <html lang="en" className={cn("font-sans", geist.variable)}>
         <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
+            {children}
         </body>
-      </html>
-    </ClerkProvider>
-  );
+    </html>
 }
+
+export default RootLayout
