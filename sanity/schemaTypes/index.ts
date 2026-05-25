5374ec709e48d53fcb67fea2376302b2044c29f3
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-25 08:43:15 +0545

    feat: add authorType schema definition and integrate into main schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType'
 import { addressType } from './addressType'
+import { authorType } from './authorType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType, addressType ],
+  types: [
+    categoryType,
+    addressType,
+    authorType],
 }
