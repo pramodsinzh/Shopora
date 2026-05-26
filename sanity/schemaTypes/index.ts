4e64e5688b1618153e10a9d0e628e4a4d8cc15d1
Pramodsnz01 <pramodsnz01@gmail.com>
2026-05-26 10:58:14 +0545

    feat: add blockContentType schema definition and integrate into main schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,10 +2,13 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType'
 import { addressType } from './addressType'
 import { authorType } from './authorType'
+import { blockContentType } from './blockContentType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     categoryType,
     addressType,
-    authorType],
+    authorType,
+    blockContentType
+  ],
 }
