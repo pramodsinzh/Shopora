7f4ad16b81b79d6de29f313bf76bd8c6ae426c38
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-15 09:49:10 +0545

    feat: add productType schema definition and integrate into main schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -6,6 +6,7 @@ import { blockContentType } from './blockContentType'
 import { blogCategoryType } from './blogCategoryType'
 import { blogType } from './blogType'
 import { brandType } from './brandType'
+import { productType } from './productType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
@@ -16,5 +17,6 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     blogCategoryType,
     blogType,
     brandType,
+    productType,
   ],
 }
