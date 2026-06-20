f1d7880a085a687d2a8537521dc288dd3e88ff45
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-20 10:21:24 +0545

    Add AddToCartButton component

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Product } from '@/sanity.types'
+import { Button } from './ui/button'
+import { ShoppingBag } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+interface Props {
+  product: Product
+  className?: string
+}
+
+const AddToCartButton = ({ product, className }: Props) => {
+  const isOutOfStock = product?.stock === 0
+  const handleAddToCart = () =>{
+    window.alert("Added to cart")
+  }
+  return (
+    <div>
+      <Button onClick={handleAddToCart} disabled={isOutOfStock} className={cn("w-full bg-shop_dark_green/80 text-shop_light_bg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect", className)}>
+        <ShoppingBag /> {isOutOfStock ? "Out Of Stock" : "Add To Cart"}
+      </Button>
+    </div>
+  )
+}
+
+export default AddToCartButton
