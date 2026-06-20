3bd04a156050052ab04d519448380563f5e8ddd3
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-20 10:21:14 +0545

    Update ProductGrid component

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,11 +4,15 @@ import { useEffect, useState } from "react"
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constants/data";
 import { client } from "@/sanity/lib/client";
-
+import { Loader2 } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import NoProductAvailable from "./NoProductAvailable";
+import ProductCard from "./ProductCard";
+import { Product } from "@/sanity.types";
 
 
 const ProductGrid = () => {
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState<Product[]>([]);
     const [loading, setLoading] = useState(false);
     const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "")
 
@@ -32,6 +36,29 @@ const ProductGrid = () => {
     return (
         <div>
             <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+            {loading ? (<div className="flex flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
+                <div className="space-x-2 flex items-center text-blue-600">
+                    <Loader2 className="w-5 h-6 animate-spin" />
+                    <span>Product is loading...</span>
+                </div>
+            </div>
+            ) : products?.length ? (
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
+                    {products?.map((product) => (
+                        <AnimatePresence key={product?._id}>
+                            <motion.div layout
+                                initial={{ opacity: 0.2 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                            >
+                                <ProductCard product={product} />
+                            </motion.div>
+                        </AnimatePresence>
+                    ))}
+                </div>
+            ) : (
+                <NoProductAvailable selectedTab={selectedTab} />
+            )}
         </div>
     )
 }
