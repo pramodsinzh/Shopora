34eac5786c78491787e2f0f2704c0d0a4ed4a743
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-22 10:27:48 +0545

    Update client home page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,14 +1,16 @@
 import Container from '@/components/Container'
 import HomeBanner from '@/components/HomeBanner'
+import HomeCategories from '@/components/HomeCategories'
 import ProductGrid from '@/components/ProductGrid'
+import { getCategories } from '@/sanity/queries'
 
-const Home = () => {
+const Home = async() => {
+  const categories = await getCategories(6) 
   return (
     <Container >
       <HomeBanner />
-      <div className="py-10">
-        <ProductGrid />
-      </div>
+      <ProductGrid />
+      <HomeCategories categories={categories} />
     </Container>
   )
 }
