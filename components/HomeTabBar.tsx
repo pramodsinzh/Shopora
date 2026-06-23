c306879b5f45598055439da6b9373538d5322f18
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-23 09:59:19 +0545

    UI fixes

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -8,12 +8,12 @@ interface Props {
 }
 
 const HomeTabBar = ({ selectedTab, onTabSelect }: Props) => {
- 
+
     return (
-        <div className=' flex flex-wrap items-center justify-between gap-5'>
+        <div className=' mt-10 flex flex-wrap items-center justify-between gap-5'>
             <div className="flex items-center gap-3 text-sm font-semibold">
-                {productType?.map((item)=> (
-                    <button onClick={()=> onTabSelect(item?.title)} className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green": "bg-shop_light_green/20"}`} key={item?.title}>{item?.title}</button>
+                {productType?.map((item) => (
+                    <button onClick={() => onTabSelect(item?.title)} className={`border border-shop_light_green/20 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect ${selectedTab === item?.title ? "bg-shop_light_green text-white border-shop_light_green" : "bg-shop_light_green/20"}`} key={item?.title}>{item?.title}</button>
                 ))}
             </div>
             <Link className={`border border-shop_light_green/30 px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-shop_light_green hover:border-shop_light_green hover:text-white hoverEffect`} href={'/shop'}>See all</Link>
