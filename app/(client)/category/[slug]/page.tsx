f35123807c71aeea5fcbe1230654724ec830d852
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-25 10:02:12 +0545

    Implement CategoryPage and add CategoryProducts component for dynamic product fetching

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import CategoryProducts from '@/components/CategoryProducts'
+import Container from '@/components/Container'
+import { SubTitle } from '@/components/ui/text'
+import { getCategories } from '@/sanity/queries' 
 
-const CategoryPage = () => {
+const CategoryPage = async({params}:{params: Promise<{slug: string}>}) => {
+  const categories = await getCategories()
+  const {slug} = await params;
   return (
-    <div>CategoryPage</div>
+    <div className='py-10'>
+      <Container >
+        <SubTitle>Products by category:{" "}
+          <span className='font-bold text-green-600 tracking-wide capitalize'>
+            {slug && slug}
+          </span>
+        </SubTitle>
+        <CategoryProducts categories={categories} slug={slug} />
+      </Container>
+    </div>
   )
 }
 
