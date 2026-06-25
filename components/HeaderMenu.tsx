455d1a28cc639a0a0d7932134daa34557ec32eda
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-25 10:05:14 +0545

    Update HeaderMenu component to center align items in the header

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const HeaderMenu = () => {
     const pathname = usePathname();
   return (
-    <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor'>
+    <div className='hidden md:inline-flex w-1/3 items-center justify-center gap-7 text-sm capitalize font-semibold text-lightColor'>
         {headerData?.map((item) => (
             <Link key={item?.title} href={item?.href} className={`hover:text-shop_light_green hoverEffect relative group ${ pathname === item?.href && "text-shop_light_green"}`}>
                 {item?.title}
