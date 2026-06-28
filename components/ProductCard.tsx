ae5b1ded5a930547d25ff6ba6219af1656aebbbd
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-28 09:21:15 +0545

    Wrap Image component in Link for product navigation in ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -14,14 +14,16 @@ const ProductCard = ({ product }: { product: Product }) => {
         <div className='text-sm border-[1px] border-dark_blue/20 rounded-md bg-white group'>
             <div className="relative group overflow-hidden bg-shop_light_bg">
                 {product?.images && (
-                    <Image
-                        src={urlFor(product?.images[0]).url()}
-                        alt="ProductImage"
-                        loading="lazy"
-                        width={700}
-                        height={700}
-                        className={`w-full h-64 object-content overflow-hidden transition-transform bg-shop_light_bg hoverEffect ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
-                    />
+                    <Link href={`/product/${product?.slug?.current}`}>
+                        <Image
+                            src={urlFor(product?.images[0]).url()}
+                            alt="ProductImage"
+                            loading="lazy"
+                            width={700}
+                            height={700}
+                            className={`w-full h-64 object-content overflow-hidden transition-transform bg-shop_light_bg hoverEffect ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
+                        />
+                    </Link>
                 )}
                 <AddToWishListButton product={product} />
                 {product?.status === "sale" && (
