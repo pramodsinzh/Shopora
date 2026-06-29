bb988c4493efce757604b1c5c5519e1c7bad1b51
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-29 17:47:43 +0545

    Update sanity/queries/query.ts

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -19,4 +19,10 @@ const PRODUCT_BY_SLUG_QUERY = defineQuery(
     `*[_type == "product" && slug.current == $slug] | order(name asc) [0]`
 )
 
-export { BRANDS_QUERY, LATEST_BLOG_QUERY, DEAL_PRODUCTS, PRODUCT_BY_SLUG_QUERY }
+const BRAND_QUERY = defineQuery(
+    `*[_type == "product" && slug.current == $slug ] | order(name asc) {
+      "brandName"  : brand -> title
+    } `
+)
+
+export { BRANDS_QUERY, LATEST_BLOG_QUERY, DEAL_PRODUCTS, PRODUCT_BY_SLUG_QUERY, BRAND_QUERY }
