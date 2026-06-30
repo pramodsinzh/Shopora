bbf2d6263f702ec01fbf6fd57f9fc45af5cfcf1c
Pramodsnz01 <pramodsnz01@gmail.com>
2026-06-30 20:46:21 +0545

    Refactor CategoryList component to display product categories with radio buttons and improve UI elements

--- components/shop/CategoryList.tsx
+++ components/shop/CategoryList.tsx
@@ -1,8 +1,39 @@
+import { Category } from '@/sanity.types'
 import React from 'react'
+import { SubTitle } from '../ui/text';
+import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
+import { Label } from '../ui/label';
 
-const CategoryList = () => {
+interface Props {
+  categories: Category[]
+  selectedCategory?: string | null
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+const CategoryList = ({ categories, selectedCategory, setSelectedCategory }: Props) => {
   return (
-    <div>CategoryList</div>
+    <div className='w-full bg-white p-5'>
+      <SubTitle className='text-base font-semibold'>Product Categories</SubTitle>
+      <RadioGroup value={selectedCategory || ""} className="mt-2 space-y-1">
+        {categories?.map((category) => (
+          <div
+            onClick={() => setSelectedCategory(category?.slug?.current as string)}
+            className="flex items-center space-x-2 cursor-pointer"
+            key={category?._id}
+          >
+            <RadioGroupItem
+              value={category?.slug?.current as string}
+              id={category?.slug?.current}
+              className="rounded-sm"
+            />
+            <Label
+              htmlFor={category?.slug?.current}
+              className={`${selectedCategory === category?.slug?.current ? "font-semibold text-shop_dark_green" : "font-normal"}`}
+            >{category?.title}</Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
   )
 }
 
