255e6a4ae4a30bd10a04a3c7c402bab40c7a88cc
Pramodsnz01 <pramodsnz01@gmail.com>
2026-07-02 09:18:14 +0545

    Implement Single Blog Page and add Zustand store for cart and favorites management

--- app/(client)/blog/[slug]/page.tsx
+++ app/(client)/blog/[slug]/page.tsx
@@ -1,9 +1,18 @@
+import Container from '@/components/Container'
+import { SubTitle } from '@/components/ui/text'
 import React from 'react'
 
-const BlogPage = () => {
+const SingleBlogPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params
+  
   return (
-    <div>BlogPage</div>
+    <div>
+      <Container>
+        <SubTitle>Single Blog Post</SubTitle>
+        <p>{slug}</p>
+      </Container>
+    </div>
   )
 }
 
-export default BlogPage
+export default SingleBlogPage
