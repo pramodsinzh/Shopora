430ba364f9389a39e98f5688aab37cd383153115
Pramodsnz01 <pramodsnz01@gmail.com>
2026-07-03 20:34:52 +0545

    Commit tracked component updates, excluding QuantityButtons.tsx

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -5,30 +5,49 @@ import { Button } from './ui/button'
 import { ShoppingBag } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import useStore from '@/store'
+import toast from 'react-hot-toast'
+import PriceFormatter from './PriceFormatter'
+import QuantityButtons from './QuantityButtons'
 
 interface Props {
-  product: Product 
+  product: Product
   className?: string
 }
 
 const AddToCartButton = ({ product, className }: Props) => {
   const { addItem, getItemCount } = useStore()
   const itemCount = getItemCount(product?._id)
-  
+
   const isOutOfStock = product?.stock === 0
   const handleAddToCart = () => {
-     if((product?.stock as number) > itemCount){
+    if ((product?.stock as number) > itemCount) {
       addItem(product)
-     }
+      toast.success(`${product?.name?.substring(0, 12)} added to cart!`)
+    } else {
+      toast.error(`Sorry, ${product?.name?.substring(0, 12)} is out of stock!`)
+    }
   }
   return (
     <div className='w-full h-12 flex items-center'>
-      <Button
-        onClick={handleAddToCart}
-        disabled={isOutOfStock}
-        className={cn("w-full bg-shop_dark_green/80 text-shop_light_bg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect", className)}>
-        <ShoppingBag /> {isOutOfStock ? "Out Of Stock" : "Add To Cart"}
-      </Button>
+      {itemCount ? (
+        <div className="text-sm w-full">
+          <div className="">
+            <span className='text-xs text-darkColor/80'>Quantity</span>
+            <QuantityButtons product={product} />
+          </div>
+          <div className="flex items-center justify-between border-t pt-1">
+            <span className='text-xs font-semibold'>Subtotal</span>
+            <PriceFormatter amount={product?.price ? product?.price * itemCount : 0} />
+          </div>
+        </div>
+      ) : (
+        <Button
+          onClick={handleAddToCart}
+          disabled={isOutOfStock}
+          className={cn("w-full bg-shop_dark_green/80 text-shop_light_bg shadow-none border border-shop_dark_green/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_green hover:border-shop_dark_green hoverEffect", className)}>
+          <ShoppingBag /> {isOutOfStock ? "Out Of Stock" : "Add To Cart"}
+        </Button>
+      )}
     </div>
   )
 }
