c8baae4821bad71d2ea9dd96140220f3e616c473
Pramodsnz01 <pramodsnz01@gmail.com>
2026-07-07 08:28:13 +0545

    Add address selection feature to CartPage with UI enhancements

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -7,6 +7,9 @@ import NoAccess from "@/components/NoAccess"
 import PriceFormatter from "@/components/PriceFormatter"
 import QuantityButtons from "@/components/QuantityButtons"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Separator } from "@/components/ui/separator"
 import { SubTitle } from "@/components/ui/text"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
@@ -30,32 +33,32 @@ const CartPage = () => {
     const groupedItems = useStore((state) => state.getGroupedItems())
     const { isSignedIn } = useAuth()
     const { user } = useUser()
-    const [addresses, setAddresses] = useState(null)
+    const [addresses, setAddresses] = useState<Address[] | null>(null)
     const [selectedAddress, setSelectedAddress] = useState<Address | null>(null)
 
-    const fetchAddresses = async() => {
+    const fetchAddresses = async () => {
         setLoading(true)
         try {
             const query = `*[_type == "address"] | order(publishedAt desc)`;
             const data = await client.fetch(query)
             setAddresses(data)
 
-            const defaultAddress = data.find((addr: any)=> addr.default)
-            if(defaultAddress){
+            const defaultAddress = data.find((addr: Address) => addr.default)
+            if (defaultAddress) {
                 setSelectedAddress(defaultAddress)
-            }else if(data.length > 0){
+            } else if (data.length > 0) {
                 setSelectedAddress(data[0])
             }
         } catch (error) {
             console.error("Address fetching error:", error)
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         fetchAddresses()
     }, [])
-    
+
     const handleClearCart = () => {
         const confirm = window.confirm("Are you sure you want to clear your cart?");
         if (confirm) {
@@ -169,6 +172,43 @@ const CartPage = () => {
                                                 </Button>
                                             </div>
                                         </div>
+                                        {addresses && (
+                                            <div className="bg-white rounded-md mt-5">
+                                                <Card>
+                                                    <CardHeader>
+                                                        <CardTitle>Delivery Address</CardTitle>
+                                                    </CardHeader>
+                                                    <CardContent>
+                                                        <RadioGroup
+                                                            defaultValue={addresses?.find((addr) => addr.default)?._id.toString()}
+                                                        >
+                                                            {addresses?.map((address) => (
+                                                                <div
+                                                                    key={address._id}
+                                                                    onClick={() => setSelectedAddress(address)}
+                                                                    className={`flex items-center space-x-2 mb-4 cursor-pointer ${selectedAddress?._id === address?._id && "text-shop_dark_green"}`}
+                                                                >
+                                                                    <RadioGroupItem value={address?._id.toString()} />
+                                                                    <Label
+                                                                        htmlFor={`address-${address?._id}`}
+                                                                        className="grid flex-1 gap-1.5"
+                                                                    >
+                                                                        <span className="font-semibold" >
+                                                                            {address?.name}
+                                                                        </span>
+                                                                        <span className="text-sm text-black/60">
+                                                                            {address.address}, {address.city},{" "}
+                                                                            {address.state} {address.zip}
+                                                                        </span>
+                                                                    </Label>
+                                                                </div>
+                                                            ))}
+                                                        </RadioGroup>
+                                                        <Button variant='outline' className='w-full mt-4'>Add New Address</Button>
+                                                    </CardContent>
+                                                </Card>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                                 {/* for mobile view */}
