647d9633e25d714add2520c0865c7010ec33a16d
Pramodsnz01 <pramodsnz01@gmail.com>
2026-07-15 17:18:12 +0545

    Updated my_orders_query

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -26,8 +26,13 @@ const BRAND_QUERY = defineQuery(
 )
 
 const MY_ORDERS_QUERY = defineQuery(
-    `*[_type == "order" && clerkUserId == $userId] | order(orderData desc){
-    ...products[]{...,product -> }}`
+    `*[_type == "order" && clerkUserId == $userId] | order(orderDate desc){
+    ...,
+    products[]{
+      ...,
+      product->
+    }
+  }`
 )
 
 export { BRANDS_QUERY, LATEST_BLOG_QUERY, DEAL_PRODUCTS, PRODUCT_BY_SLUG_QUERY, BRAND_QUERY, MY_ORDERS_QUERY }
